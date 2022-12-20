a8450e0891654d6a8476c09111d530300cbe007e
Samer A <thedev.samer@gmail.com>
2022-12-20 18:32:20 +0200

    finished scraping top scorers and top pasers

--- main.py
+++ main.py
@@ -1,16 +1,73 @@
-# This is a sample Python script.
+# import external modules
+import openpyxl
+import requests
+from bs4 import BeautifulSoup
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+# create an Excel file
+excel = openpyxl.Workbook()
+
+# make sure that we are working on the active sheet
+sheet = excel.active
+
+# set the name of the sheet
+sheet.title = "Football Database"
+
+# requesting the IMDB website
+url = "https://www.footballdatabase.eu/en/players"
+source = requests.get(url)
+
+# throws an error in case of the URL has issues
+source.raise_for_status()
+
+# read and parse HTML file
+soup = BeautifulSoup(source.text, "html.parser")
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+# scrape the best worldwide scorers
+def scrape_best_scorers():
+    # read table rows
+    rows = soup.find_all("div", class_="pbestscorers")[0].find_all("tr", class_="line")
+
+    # write table head
+    sheet.append(["Players", "Goals"])
+
+    for row in rows:
+        # read player child element
+        player_name = row.find("td", class_="player").get_text(strip=True)
+
+        # read score element
+        player_score = row.find("td", class_="score").get_text(strip=True)
+
+        # write a new row to the current table
+        sheet.append([player_name, player_score])
+
+    # write a break line in the sheet
+    sheet.append(["", ""])
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+def scrape_best_passers():
+    # read table rows
+    rows = soup.find_all("div", class_="pbestscorers")[1].find_all("tr", class_="line")
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    # write table head
+    sheet.append(["Players", "Assists"])
+
+    for row in rows:
+        # read player child element
+        player_name = row.find("td", class_="player").get_text(strip=True)
+
+        # read score element
+        player_score = row.find("td", class_="score").get_text(strip=True)
+
+        # write a new row to the current table
+        sheet.append([player_name, player_score])
+
+    # write a break line in the sheet
+    sheet.append(["", ""])
+
+
+scrape_best_scorers()
+scrape_best_passers()
+
+# save Excel file
+excel.save("Football Database.xlsx")
